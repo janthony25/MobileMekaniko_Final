4f86673022eccffc4304d5f21a976875f0d38434
Jun Anthony <junanthony.business@gmail.com>
2024-10-04 17:38:33 +1300

    Add UpdateInvoiceDto

---
+++ MobileMekaniko_Final/Models/Dto/UpdateInvoiceDto.cs
@@ -0,0 +1,18 @@
+﻿namespace MobileMekaniko_Final.Models.Dto
+{
+    public class UpdateInvoiceDto
+    {
+        public int InvoiceId { get; set; }
+        public required string IssueName { get; set; }
+        public string? PaymentTerm { get; set; }
+        public string? Notes { get; set; }
+        public decimal? LaborPrice { get; set; }
+        public decimal? Discount { get; set; }
+        public decimal? ShippingFee { get; set; }
+        public decimal? SubTotal { get; set; }
+        public decimal? TotalAmount { get; set; }
+        public decimal? AmountPaid { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime DateEdited { get; set; } = DateTime.Now;
+    }
+}
