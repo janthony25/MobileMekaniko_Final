c083a909ed9a410f3f2d040b4f772cae4aa4d24d
Jun Anthony <junanthony.business@gmail.com>
2024-10-12 15:08:56 +1300

    Fixed pie chart color

--- MobileMekaniko_Final/wwwroot/js/dashboard.js
+++ MobileMekaniko_Final/wwwroot/js/dashboard.js
@@ -27,14 +27,14 @@ function initializePieChart() {
                     datasets: [{
                         data: [data.totalInvoiceAmount, data.totalPaidAmount, data.remainingBalance],
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.8)',
+                            'rgba(54, 162, 235, 0.8)',
                             'rgba(75, 192, 192, 0.8)',
-                            'rgba(255, 206, 86, 0.8)'
+                            'rgba(255, 99, 132, 0.8)'
                         ],
                         borderColor: [
-                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
                             'rgba(75, 192, 192, 1)',
-                            'rgba(255, 206, 86, 1)'
+                            'rgba(255, 99, 132, 1)'
                         ],
                         borderWidth: 1
                     }]
